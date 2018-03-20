90e03de20447b9dfcc74acf7ef611fdb86ce29a8
Aaron Stevens <bheklilr2@gmail.com>
2018-03-20 10:57:49 -0500

    Still trying

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 export FLASK_APP=autoapp.py
 export FLASK_DEBUG=0
-npm run build
-gunicorn twitter_python_mentors.app:create_app() -b 0.0.0.0:$PORT -w 3
+npm run-script build-colorless
+npm run-script gunicorn-server
